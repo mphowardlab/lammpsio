0f2bb7e3bcb3d4b511a7a1622cfcf9141eee2468
clpetix <clpetix@gmail.com>
2024-04-07 11:03:01 -0500

    Test read and write topology data to file.

--- tests/test_data_file.py
+++ tests/test_data_file.py
@@ -103,3 +103,91 @@ def test_data_file_all(snap, atom_style, set_style, shuffle_ids, tmp_path):
         assert numpy.allclose(snap_2.charge, snap.charge)
     else:
         assert not snap_2.has_charge()
+
+
+def test_data_file_topology(snap_top, tmp_path):
+    # particle information
+    snap_top.id = [1, 2, 3, 4, 5, 6, 7, 8]
+    snap_top.typeid = [1, 1, 1, 1, 2, 2, 2, 2]
+    snap_top.position = [
+        [0, 0, 0],
+        [0.1, 0.1, 0.1],
+        [0.2, 0.2, 0.2],
+        [0.3, 0.3, 0.3],
+        [1, 1, 1],
+        [1.1, 1.1, 1.1],
+        [1.2, 1.2, 1.2],
+        [1.3, 1.3, 1.3],
+    ]
+    snap_top.mass = [1, 1, 1, 1, 2, 2, 2, 2]
+
+    # bond information
+    snap_top.bonds = lammpsio.topology.Bonds(N=6, num_types=2)
+    snap_top.bonds.id = [1, 2, 3, 4, 5, 6]
+    snap_top.bonds.typeid = [1, 1, 1, 2, 2, 2]
+    snap_top.bonds.members = [
+        [1, 2],
+        [2, 3],
+        [3, 4],
+        [5, 6],
+        [6, 7],
+        [7, 8],
+    ]
+
+    # angle information
+    snap_top.angles = lammpsio.topology.Angles(N=4, num_types=2)
+    snap_top.angles.id = [1, 2, 3, 4]
+    snap_top.angles.typeid = [1, 1, 2, 2]
+    snap_top.angles.members = [
+        [1, 2, 3],
+        [2, 3, 4],
+        [5, 6, 7],
+        [6, 7, 8],
+    ]
+
+    # dihedral information
+    snap_top.dihedrals = lammpsio.topology.Dihedrals(N=2, num_types=2)
+    snap_top.dihedrals.id = [1, 2]
+    snap_top.dihedrals.typeid = [1, 2]
+    snap_top.dihedrals.members = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+    ]
+
+    filename = tmp_path / "atoms.data"
+    data = lammpsio.DataFile.create(filename, snap_top)
+    assert filename.exists
+
+    snap_2 = data.read()
+    assert snap_2.N == snap_top.N
+    assert not snap_2.has_id()
+    assert numpy.allclose(snap_2.id, snap_top.id)
+    assert snap_2.has_typeid()
+    assert numpy.allclose(snap_2.typeid, snap_top.typeid)
+    assert snap_2.has_position()
+    assert numpy.allclose(snap_2.position, snap_top.position)
+    assert snap_2.has_mass()
+    assert numpy.allclose(snap_2.mass, snap_top.mass)
+    assert snap_2.bonds.N == snap_top.bonds.N
+    assert snap_2.bonds.has_id()
+    assert numpy.allclose(snap_2.bonds.id, snap_top.bonds.id)
+    assert snap_2.bonds.has_typeid()
+    assert numpy.allclose(snap_2.bonds.typeid, snap_top.bonds.typeid)
+    assert snap_2.bonds.has_members()
+    assert numpy.allclose(snap_2.bonds.members, snap_top.bonds.members)
+
+    assert snap_2.angles.N == snap_top.angles.N
+    assert snap_2.angles.has_id()
+    assert numpy.allclose(snap_2.angles.id, snap_top.angles.id)
+    assert snap_2.angles.has_typeid()
+    assert numpy.allclose(snap_2.angles.typeid, snap_top.angles.typeid)
+    assert snap_2.angles.has_members()
+    assert numpy.allclose(snap_2.angles.members, snap_top.angles.members)
+
+    assert snap_2.dihedrals.N == snap_top.dihedrals.N
+    assert snap_2.dihedrals.has_id()
+    assert numpy.allclose(snap_2.dihedrals.id, snap_top.dihedrals.id)
+    assert snap_2.dihedrals.has_typeid()
+    assert numpy.allclose(snap_2.dihedrals.typeid, snap_top.dihedrals.typeid)
+    assert snap_2.dihedrals.has_members()
+    assert numpy.allclose(snap_2.dihedrals.members, snap_top.dihedrals.members)
