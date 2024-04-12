8b167308c164122c55d21d234e85ce7f7cfabeec
clpetix <clpetix@gmail.com>
2024-04-12 12:18:36 -0500

    Add shuffle ideas to topology test.

--- tests/test_data_file.py
+++ tests/test_data_file.py
@@ -105,9 +105,24 @@ def test_data_file_all(snap, atom_style, set_style, shuffle_ids, tmp_path):
         assert not snap_2.has_charge()
 
 
-def test_data_file_topology(snap_top, tmp_path):
+@pytest.mark.parametrize("shuffle_ids", [False, True])
+def test_data_file_topology(snap_top, tmp_path, shuffle_ids):
+    # set ids to be assigned
+    if shuffle_ids:
+        particle_id = [1, 5, 2, 6, 3, 7, 4, 8]
+        bond_id = [1, 4, 2, 5, 3, 6]
+        angle_id = [1, 3, 2, 4]
+        dihedral_id = [2, 1]
+        improper_id = [2, 1]
+    else:
+        particle_id = [1, 2, 3, 4, 5, 6, 7, 8]
+        bond_id = [1, 2, 3, 4, 5, 6]
+        angle_id = [1, 2, 3, 4]
+        dihedral_id = [1, 2]
+        improper_id = [1, 2]
+
     # particle information
-    snap_top.id = [1, 2, 3, 4, 5, 6, 7, 8]
+    snap_top.id = particle_id
     snap_top.typeid = [1, 1, 1, 1, 2, 2, 2, 2]
     snap_top.position = [
         [0, 0, 0],
@@ -123,8 +138,8 @@ def test_data_file_topology(snap_top, tmp_path):
 
     # bond information
     snap_top.bonds = lammpsio.topology.Bonds(N=6, num_types=2)
-    snap_top.bonds.id = [1, 2, 3, 4, 5, 6]
-    snap_top.bonds.typeid = [1, 1, 1, 2, 2, 2]
+    snap_top.bonds.id = bond_id
+    snap_top.bonds.typeid = [1, 2, 1, 2, 1, 2]
     snap_top.bonds.members = [
         [1, 2],
         [2, 3],
@@ -136,8 +151,8 @@ def test_data_file_topology(snap_top, tmp_path):
 
     # angle information
     snap_top.angles = lammpsio.topology.Angles(N=4, num_types=2)
-    snap_top.angles.id = [1, 2, 3, 4]
-    snap_top.angles.typeid = [1, 1, 2, 2]
+    snap_top.angles.id = angle_id
+    snap_top.angles.typeid = [1, 2, 2, 1]
     snap_top.angles.members = [
         [1, 2, 3],
         [2, 3, 4],
@@ -147,9 +162,18 @@ def test_data_file_topology(snap_top, tmp_path):
 
     # dihedral information
     snap_top.dihedrals = lammpsio.topology.Dihedrals(N=2, num_types=2)
-    snap_top.dihedrals.id = [1, 2]
+    snap_top.dihedrals.id = dihedral_id
     snap_top.dihedrals.typeid = [1, 2]
     snap_top.dihedrals.members = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+    ]
+
+    # improper information
+    snap_top.impropers = lammpsio.topology.Impropers(N=2, num_types=2)
+    snap_top.impropers.id = improper_id
+    snap_top.impropers.typeid = [1, 2]
+    snap_top.impropers.members = [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
     ]
@@ -159,8 +183,13 @@ def test_data_file_topology(snap_top, tmp_path):
     assert filename.exists
 
     snap_2 = data.read()
+    # test none topology features of a snapshot with topology
     assert snap_2.N == snap_top.N
-    assert not snap_2.has_id()
+    if shuffle_ids:
+        assert snap_2.has_id()
+        assert numpy.allclose(snap_2.id, snap_top.id)
+    else:
+        assert not snap_2.has_id()
     assert numpy.allclose(snap_2.id, snap_top.id)
     assert snap_2.has_typeid()
     assert numpy.allclose(snap_2.typeid, snap_top.typeid)
@@ -168,6 +197,8 @@ def test_data_file_topology(snap_top, tmp_path):
     assert numpy.allclose(snap_2.position, snap_top.position)
     assert snap_2.has_mass()
     assert numpy.allclose(snap_2.mass, snap_top.mass)
+
+    # test bonds
     assert snap_2.bonds.N == snap_top.bonds.N
     assert snap_2.bonds.has_id()
     assert numpy.allclose(snap_2.bonds.id, snap_top.bonds.id)
@@ -176,6 +207,7 @@ def test_data_file_topology(snap_top, tmp_path):
     assert snap_2.bonds.has_members()
     assert numpy.allclose(snap_2.bonds.members, snap_top.bonds.members)
 
+    # test angles
     assert snap_2.angles.N == snap_top.angles.N
     assert snap_2.angles.has_id()
     assert numpy.allclose(snap_2.angles.id, snap_top.angles.id)
@@ -184,6 +216,7 @@ def test_data_file_topology(snap_top, tmp_path):
     assert snap_2.angles.has_members()
     assert numpy.allclose(snap_2.angles.members, snap_top.angles.members)
 
+    # test dihedrals
     assert snap_2.dihedrals.N == snap_top.dihedrals.N
     assert snap_2.dihedrals.has_id()
     assert numpy.allclose(snap_2.dihedrals.id, snap_top.dihedrals.id)
@@ -191,3 +224,12 @@ def test_data_file_topology(snap_top, tmp_path):
     assert numpy.allclose(snap_2.dihedrals.typeid, snap_top.dihedrals.typeid)
     assert snap_2.dihedrals.has_members()
     assert numpy.allclose(snap_2.dihedrals.members, snap_top.dihedrals.members)
+
+    # test impropers
+    assert snap_2.impropers.N == snap_top.impropers.N
+    assert snap_2.impropers.has_id()
+    assert numpy.allclose(snap_2.impropers.id, snap_top.impropers.id)
+    assert snap_2.impropers.has_typeid()
+    assert numpy.allclose(snap_2.impropers.typeid, snap_top.impropers.typeid)
+    assert snap_2.impropers.has_members()
+    assert numpy.allclose(snap_2.impropers.members, snap_top.impropers.members)
