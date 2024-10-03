ed578b1cbd9a5dd5dcc64c3fcf65754b32278f68
clpetix <clpetix@gmail.com>
2024-10-03 16:10:45 -0500

    Simplify setting types for particles.

--- src/lammpsio/snapshot.py
+++ src/lammpsio/snapshot.py
@@ -258,33 +258,21 @@ class Snapshot:
             frame.particles.image = self.image.copy()
         if self.has_typeid():
             frame.particles.typeid = numpy.zeros(self.N, dtype=int)
-            if type_map is None:
-                if self.type_label is not None:
-                    type_label_map = self.type_label
-                    frame.particles.types = type_label_map.types
-                    _set_type_id(
-                        self.typeid,
-                        frame.particles.typeid,
-                        type_label_map,
-                    )
-                else:
-                    sorted_typeids = numpy.sort(numpy.unique(self.typeid))
-                    frame.particles.types = [str(typeid) for typeid in sorted_typeids]
-                    for typeidx, typeid in enumerate(sorted_typeids):
-                        frame.particles.typeid[self.typeid == typeid] = typeidx
-            else:
+            if type_map is not None:
                 warnings.warn(
                     "type_map is deprecated, use label instead.",
                     DeprecationWarning,
                     stacklevel=2,
                 )
                 type_label_map = LabelMap(map=type_map)
-                frame.particles.types = type_label_map.types
-                _set_type_id(
-                    self.typeid,
-                    frame.particles.typeid,
-                    type_label_map,
+            else:
+                type_label_map = self.type_label
+            if self.has_typeid():
+                frame.particles.typeid = numpy.zeros(self.N, dtype=int)
+                type_label_map = _set_type_id(
+                    self.typeid, frame.particles.typeid, type_label_map
                 )
+            frame.particles.types = type_label_map.types
         if self.has_charge():
             frame.particles.charge = self.charge.copy()
         if self.has_mass():
