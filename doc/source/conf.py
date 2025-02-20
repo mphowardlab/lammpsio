7950645e572e56bc7b4fb405ac9dd60e99447291
clpetix <clpetix@gmail.com>
2025-02-20 15:02:00 -0600

    Set up basic documentation outline.

---
+++ doc/source/conf.py
@@ -0,0 +1,67 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import datetime
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src"))
+
+# -- Project information -----------------------------------------------------
+
+project = "lammmpsio"
+year = datetime.date.today().year
+copyright = f"2021-{year}, Auburn University"
+author = "Michael P. Howard"
+version = "0.2.1"
+release = "0.2.1"
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "IPython.sphinxext.ipython_console_highlighting",
+    "nbsphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx_design",
+    "sphinx_favicon",
+]
+
+
+templates_path = ["_templates"]
+
+exclude_patterns = []
+
+default_role = "any"
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "furo"
+html_static_path = ["_static"]
+html_theme_options = {
+    "navigation_with_keys": True,
+    "top_of_page_buttons": [],
+    "dark_css_variables": {
+        "color-brand-primary": "#ef904d",
+        "color-brand-content": "#ef904d",
+        "color-admonition-background": "#ef904d",
+    },
+    "light_css_variables": {
+        "color-brand-primary": "#e86100",
+        "color-brand-content": "#e86100",
+        "color-admonition-background": "#e86100",
+    },
+}
+
+# -- Options for autodoc & autosummary ---------------------------------------
+
+autosummary_generate = True
+
+autodoc_default_options = {"inherited-members": None, "special-members": False}
---
+++ doc/source/index.rst
@@ -0,0 +1,17 @@
+lammpsio documentation
+======================
+
+Tools for working with LAMMPS data and dump files.
+
+
+.. toctree::
+    :maxdepth: 1
+    :caption: User guide
+
+    guide/index
+
+.. toctree::
+    :maxdepth: 1
+    :caption: API reference
+
+    api/index
