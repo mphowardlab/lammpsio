fed1ff13ea82865a2b5f30134167aeb6b4ebbe40
clpetix <clpetix@gmail.com>
2025-02-21 10:16:44 -0600

    Fix warnings in Sphinx.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -71,4 +71,5 @@ autodoc_default_options = {"inherited-members": None, "special-members": False}
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "numpy": ("https://numpy.org/doc/stable", None),
+    "gsd": ("https://gsd.readthedocs.io/en/stable/", None),
 }
