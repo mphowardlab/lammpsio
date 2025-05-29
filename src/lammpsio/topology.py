b3d164047902dc57202ff31aa4a0de13249d4a91
mayukhkundu14 <79770572+mayukh33@users.noreply.github.com>
2025-05-29 11:05:28 -0500

    Modify topology module documentation

--- src/lammpsio/topology.py
+++ src/lammpsio/topology.py
@@ -5,6 +5,27 @@ and `Impropers` objects. All these objects function similarly, differing only in
 number of particles that are included in a connection (2 for a bond, 3 for an angle,
 4 for a dihedral or improper). Each connection has an associated ``id`` and ``typeid``.
 
+.. code-block:: python
+
+    bonds = lammpsio.topology.Bonds(N=3, num_types=2)
+    angles = lammpsio.topology.Angles(N=2, num_types=1)
+
+These constructor arguments are available as attributes:
+
+- ``N``: number of connections (int)
+- ``num_types``: number of connection types (int). If ``num_types is None``, then the number of types is deduced from ``typeid``.
+
+The data contained per connection is:
+- ``members``: (*N*, *M*) array of particles IDs in each topology (dtype: `int`, default: ``1``),
+where *M* is the number of particles in a connection.
+- ``id``: (*N*,) array topology IDs (dtype: `int`, default: runs from 1 to *N*)
+- ``typeid``: (*N*,) array of type indexes (dtype: `int`, default: ``1``)
+
+A label (type) can be associated with a connection's typeid using a ``type_label``.
+- ``type_label``: Labels of connection typeids. (`LabelMap`, default: `None`)
+
+All values of indexes will follow the LAMMPS 1-indexed convention, but the
+arrays themselves are 0-indexed. Lazy array initialization is used as for the `Snapshot`.
 """
 
 import collections.abc
